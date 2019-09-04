2cdd41d9a4bf08d26e4d37ac16d4223e9317e034
NohYujeong <shdbwjd0326@naver.com>
2019-09-04 18:16:40 +0900

    Add freshdesk connect method

--- index.ts
+++ index.ts
@@ -9,10 +9,27 @@ export interface FeedbackTicket {
   createdAt?: string;
 }
 
+export interface FreshdeskTicket {
+  email: string;
+  subject: string;
+  description: string;
+  status: number;
+  priority: number;
+  source: number;
+}
+
 export default class Feedback {
   private feedbackPostingUrl =
-    "https://qg6wp4ze48.execute-api.us-east-1.amazonaws.com/prod/feedbacks/new";
+    "https://qg6wp4ze48.execute-api.us-east-1.amazonaws.com/prod";
+
   public async sendFeedback(feedbackTicket: FeedbackTicket) {
-    await axios.post(this.feedbackPostingUrl, feedbackTicket);
+    await axios.post(
+      this.feedbackPostingUrl + "/feedbacks/new",
+      feedbackTicket
+    );
+  }
+
+  public async sendTicketToFreshdesk(freshdeskTicket: FreshdeskTicket) {
+    await axios.post(this.feedbackPostingUrl + "/ticket/new", freshdeskTicket);
   }
 }
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -6,7 +6,16 @@ export interface FeedbackTicket {
     gaId?: string;
     createdAt?: string;
 }
+export interface FreshdeskTicket {
+    email: string;
+    subject: string;
+    description: string;
+    status: number;
+    priority: number;
+    source: number;
+}
 export default class Feedback {
     private feedbackPostingUrl;
     sendFeedback(feedbackTicket: FeedbackTicket): Promise<void>;
+    sendTicketToFreshdesk(freshdeskTicket: FreshdeskTicket): Promise<void>;
 }
